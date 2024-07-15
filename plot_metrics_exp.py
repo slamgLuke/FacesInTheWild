5eff0bdee925a1648881480775edf2342a7e38df
DavidHerencia <david.herencia@utec.edu.pe>
2024-07-15 00:22:59 -0500

    siamese con resnet 50

--- plot_metrics_exp.py
+++ plot_metrics_exp.py
@@ -1,27 +1,7 @@
 import matplotlib.pyplot as plt
-from sklearn.metrics import roc_curve, auc
 import seaborn as sns
 import pandas as pd
 import numpy as np
-
-def plot_roc_curve(y_true, y_pred):
-    # Compute ROC curve and ROC area for each class
-    fpr, tpr, _ = roc_curve(y_true, y_pred)
-    roc_auc = auc(fpr, tpr)
-
-    # Plot ROC curve
-    plt.figure()
-    plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'ROC curve (area = {roc_auc:0.2f})')
-    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Curve ROC(Receiver Operating Characteristic)')
-    plt.legend(loc="lower right")
-    plt.show()
-
-
 
 def plot_metrics(accuracy, precision, recall, f1, labels):
     # Extraer los resultados de los scores
@@ -45,9 +25,8 @@ def plot_metrics(accuracy, precision, recall, f1, labels):
     # Barras
     sns.barplot(x="Labels", y="Score", hue="Metric", data=df, ax=ax, width=0.3)
     
-    # Ajustar los límites del eje y
-    y_limit_inf = df["Score"].min()
-    ax.set_ylim(y_limit_inf, 1)
+    
+    ax.set_ylim(0.5, 1)
 
     # Ajustar las etiquetas y el diseño del gráfico
     ax.set_xlabel("Labels", fontsize=14)
@@ -64,11 +43,17 @@ def plot_metrics(accuracy, precision, recall, f1, labels):
     plt.tight_layout()  # Ajustar automáticamente los márgenes de la figura
     plt.show()
 
-# Ejemplo de uso
-accuracy = [0.95, 0.92]  # Ejemplo de accuracy para cada combinación
-precision = [0.90, 0.85]  # Ejemplo de precision para cada combinación
-recall = [0.95, 0.92]  # Ejemplo de recall para cada combinación
-f1 = [0.92, 0.88]  # Ejemplo de f1 para cada combinación
+# Resultados de experimentacion
+triplet_nn =      [0.74, 0.79, 0.83, 0.81]
+facenet_pytorch = [0.97, 0.98, 0.97, 0.99]
+resnet50 =        [0.58, 0.58, 0.58, 0.58] 
+resnet152 =       [0.69, 0.69, 0.69, 0.69]
+alexnet =         [0.6,  0.6,   0.6, 0.6]
 
-labels = ["Resnet", "Facenet"]  # Ejemplo de etiquetas para cada combinación
+accuracy =  [0.74, 0.97, 0.58, 0.69, 0.6]   
+precision = [0.79, 0.98, 0.58, 0.69, 0.6]  
+recall =    [0.83, 0.97, 0.58, 0.69, 0.6] 
+f1 =        [0.81, 0.99, 0.58, 0.69, 0.6]
+
+labels = ["triplet_nn", "facenet_pytorch", "resnet50", "resnet152", "alexnet"]  # Ejemplo de etiquetas para cada combinación
 plot_metrics(accuracy, precision, recall, f1, labels)
